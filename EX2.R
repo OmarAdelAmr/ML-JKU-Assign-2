1fc76e80469e21767c2e9851ebac13977454832e
Omar Amr <omaradel9545@gmail.com>
2017-11-20 17:54:16 +0100

    [ADD] Ex3 added, reports, and submission files

--- EX2.R
+++ EX2.R
@@ -1,17 +1,21 @@
+# Name: Omar Amr
+# Matrikel-Nr: k11776960
+
 prob_neg = 0.75
 prob_pos = 0.25
 
 x <- seq(-10, 10, 0.1)
-
 x_neg_prob =  (exp(-((x + 1) ^ 2) / 4)) / (2*sqrt(pi))
 x_pos_prob =  (sqrt(2) * exp(-2 * (x - 3) ^ 2)) / sqrt(pi)
 
 px = (x_neg_prob * prob_neg) + (x_pos_prob * prob_pos)
-
 py_pos_given_x <- x_pos_prob * prob_pos / px
 py_neg_given_x <- x_neg_prob * prob_neg / px
 
-plot(x, py_neg_given_x,type = "l", col="red", xlab = "x Values", ylab = "p(y|x)",
-     main = "p(y=-1|x) vs. p(y=+1|x)", cex.lab=1.2, cex.axis = 1.2, cex=1.2, xaxt="n")
+plot(x, px, type="l", col="blue", xlab="x Values", ylab="p(x)", main="p(x) Visualization")
+
+plot(x, py_neg_given_x, type="l", col="red", xlab="x Values", ylab="p(y|x)",
+     main="p(y=-1|x) vs. p(y=+1|x)", cex.lab=1.2, cex.axis=1.2, cex=1.2, xaxt="n")
 lines(x, py_pos_given_x, col="blue")
 axis(side = 1, at = seq(-10, 10))
+legend("left", "center", legend=c("p(y=-1|x) ", "p(y=+1|x)"), col=c("red", "blue"), lty=1:1, cex=0.8)
--- EX3.R
+++ EX3.R
@@ -1,3 +1,8 @@
+# Name: Omar Amr
+# Matrikel-Nr: k11776960
+
+set.seed(4000)
+
 create_training_set <- function(set_size, start_value, end_value)
 {
   return(runif(set_size, start_value, end_value))
@@ -13,19 +18,22 @@ y <- evaluate_training_set(x)
 noise <- rnorm(200, mean = 0, sd = sqrt(0.09))
 y_with_noise <- y + noise
 
-plot(x,y_with_noise,col='blue',xlab='x',main='TODO')
-points(x,y,col='red')
-
+# plot(x,y_with_noise,col='blue',xlab='x',main='TODO')
+# points(x,y,col='red')
 ######################################################
+
 training_set <- create_training_set(4000, -1, 3)
-# training_set_results <- evaluate_training_set(training_set)
 x0_correct_value <- evaluate_training_set(1.8)
   
-
-error <- vector()
+bias <- vector()
+variance <- vector()
+estimated_value_variance <- vector()
+EPE <- vector()
 for (n in seq(1, 7))
 {
-  error_n <- 0
+  bias_per_n <- vector()
+  variance_per_n <- vector()
+  variance_per_set <- vector()
   for (counter in seq(1, 200))
   {
     startIndex <- (20 * (counter - 1)) + 1
@@ -34,13 +42,18 @@ for (n in seq(1, 7))
     current_training_set_results <- evaluate_training_set(current_training_set)
     model <- lm(current_training_set_results ~ poly(current_training_set,n))
     predicted_value <- predict(model, newdata = data.frame(current_training_set=1.8))
-    error_n <- error_n + ((x0_correct_value - predicted_value) ^ 2)
+    bias_per_n[counter] <- predicted_value
+    variance_per_n[counter] <- (predicted_value - mean(predicted_value)) ^ 2
+    variance_per_set[counter] <- predicted_value
   }
-  error[n] <- error_n/400
+  bias[n] <- (mean(bias_per_n) - x0_correct_value) ^ 2
+  variance[n] <- mean(variance_per_n)
+  estimated_value_variance[n] <- var(variance_per_set)
+  EPE[n] <- bias[n] + variance[n] + estimated_value_variance[n]
 }
 
-plot(seq(1, 7),error,col='blue',xlab='x',main='TODO', type = "l")#EPE
-lines(seq(1, 7),error/1.5,col='red',xlab='x',main='TODO', type = "l")
-lines(seq(1, 7),error/3,col='green',xlab='x',main='TODO', type = "l")
-
-
+plot(seq(1, 7), EPE, col='red', xlab="n (Model's Polynomial Degree)", ylab = "EPE,   Bias^2,  Variance, etc", main='EX3 - Bias-Variance Decomposition', type = "l")
+lines(seq(1, 7), bias, col='blue')
+lines(seq(1, 7), estimated_value_variance, col='green')
+lines(seq(1, 7), variance, col='orange')
+legend("topright", legend=c("EPE", "Bias ^ 2", "Variance in Estimated Values", "Variance(y|x0)"), col=c("red", "blue", "green", "orange"), lty=1:1, cex=0.8)
